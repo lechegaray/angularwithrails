34933b8e6822f4e697a7592607234c86daa2f1d9
Luis Echegaray <mrluisechegaray@gmail.com>
2015-07-06 17:31:28 -0400

    adding styles

--- app.js
+++ app.js
@@ -5,14 +5,23 @@ angular.module('flapperNews', [])
             $scope.test = 'Hello world!';
             $scope.posts = [
                 {title: 'post 1', upvotes: 5},
-                {title: 'post 1', upvotes: 7},
-                {title: 'post 1', upvotes: 1},
-                {title: 'post 1', upvotes: 2},
-                {title: 'post 1', upvotes: 5}
+                {title: 'post 2', upvotes: 7},
+                {title: 'post 3', upvotes: 1},
+                {title: 'post 4', upvotes: 2},
+                {title: 'post 5', upvotes: 5}
             ];
             $scope.addPost = function(){
-                $scope.posts.push({title: $scope.title, upvotes: 0});
-                $scope.title = '';
+                if (!$scope.title || $scope.title === '') { return;};
+                $scope.posts.push({
+                    title: $scope.title, 
+                    link: $scope.link,
+                    upvotes: 0
+                });
+                $scope.title = ''; //we then clear after the post has been created
+                $scope.link= '';
+            };
+            $scope.incrementUpvotes = function(post) {
+                post.upvotes += 1;
             };
         }]);
 
